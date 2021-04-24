0c89bcdc531b3a145ca9fbb8c63e1b4f5300f512
Guetso <legalloisprovensal@gmail.com>
2021-04-24 12:35:52 +0200

    rm rootPath function

--- config.php
+++ config.php
@@ -1,5 +1,4 @@
 <?php
 require 'vendor/autoload.php';
-require 'utils/rootPath.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
--- utils/rootPath.php
+++
@@ -1,6 +0,0 @@
-<?php
-function getAddress()
-{
-    $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
-    return $protocol . '://' . $_SERVER['HTTP_HOST'] . '/';
-}
