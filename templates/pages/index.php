db314bde427dbc60cb237d198e18b2761df5056f
Guetso <legalloisprovensal@gmail.com>
2021-04-28 12:00:11 +0200

    fix relative path

--- templates/pages/index.php
+++ templates/pages/index.php
@@ -1,8 +1,8 @@
 <?php ob_start(); ?>
     <h1><?= $pageTitle ?></h1>
-    <a href="../../posts.php">Voir les articles</a>
+    <a href="./posts.php">Voir les articles</a>
     <br/>
-    <a href="../../signup.php">S'inscrire</a>
+    <a href="./signup.php">S'inscrire</a>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('templates/template.php'); ?>
