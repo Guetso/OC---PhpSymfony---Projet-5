7bd1b4cf45f66b35642fdc17880bca2d8121ee0f
Guetso <legalloisprovensal@gmail.com>
2021-05-18 17:39:38 +0200

    fix css path

--- view/template.php
+++ view/template.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?></title>
-    <link href="<?= __DIR__.'/public/style.css' ?>" rel="stylesheet"/>
+    <link href="<?= 'public/style.css' ?>" rel="stylesheet"/>
 </head>
 
 <body>
